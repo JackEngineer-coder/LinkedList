8a6dc5af5cda53b445216d5ddce394a06db6cdba
Jitender Chauhan <66746889+JackEngineer-coder@users.noreply.github.com>
2022-01-22 19:30:43 +0530

    Create LinkedList Data Member.java
    Introduction to linked list. in this, we have created a data member of the linked list that is Node class and Linked list class.

---
+++ LinkedList Data Member.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static class Node{
+        int data;
+        Node next;
+    }
+    public static class LinkedList{
+        Node head;
+        Node tail;
+        int size;
+    }
+    public static void main(String[] args) throws Exception{
+        
+    }
+}
